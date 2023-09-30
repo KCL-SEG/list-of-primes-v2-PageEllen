965b51182ae03eee88d883ce3eed252db478be82
PageEllen <ellen.page@kcl.ac.uk>
2023-09-30 12:57:27 +0100

    first attempt at challenge

--- primes.py
+++ primes.py
@@ -1,6 +1,46 @@
+
+
+
+
 """List of prime numbers generator."""
+
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
 def primes(number_of_primes):
+
+    if number_of_primes < 1:
+
+        raise ValueError("the number of primes must be greater than 0")
+
     list = []
+
+    candidate = 1
+
+    while len(list) < number_of_primes:
+
+        prime = True
+
+        i = 2
+
+        while prime == True and i <= candidate/2:
+
+            if candidate%i == 0:
+
+                prime = False
+
+            else:
+
+                i += 1
+
+        if prime == True:
+
+            list.add(candidate)
+
+        candidate += 1
+
+        
+
     return list
+
